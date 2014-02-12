e71a0871202e1f9ee81c41e0309f672d989ff0ec
Jakob Murko <jakob@qy.si>
2014-02-12 11:51:39 +0100

    Fixes...

--- example/simple.js
+++ example/simple.js
@@ -11,4 +11,14 @@ builder.addKpi("birko", 1234);
 builder.addKpi("cirko", 1234);
 builder.addKpi("sirko", 1234);
 
+pipeline = builder.createPipeline();
+pipeline.setKey("testpipeline");
+pipeline.setDate();
+pipeline.addSlice("China", 123);
+pipeline.addSlice("EU", 3321);
+pipeline.addSlice("USA", 9876);
+builder.addWidget(pipeline);
+
 client.pushData(builder);
+
+client.getLog();
