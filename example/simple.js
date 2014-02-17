4a200c0038a694d125673de374dfd0b2639c70dc
Jakob Murko <jakob@qy.si>
2014-02-17 15:24:48 +0100

    Changes...

--- example/simple.js
+++ example/simple.js
@@ -3,22 +3,90 @@ var databox = require('../lib/databox')
 var builder
 var client
 
-builder = databox.createBuilder();
-client  = databox.createClient("5x8m7rwomz8c0wo8", "61usps63aug48k8ow8kkkw0gckwkck0o");
+builder = databox.createBuilder()
+client  = databox.createClient("5x8m7rwomz8c0wo8", "61usps63aug48k8ow8kkkw0gckwkck0o")
 
-builder.addKpi("mirko", 1234);
-builder.addKpi("birko", 1234);
-builder.addKpi("cirko", 1234);
-builder.addKpi("sirko", 1234);
+//Simple KPI's
+builder.addKpi("mirko", 1234)
+builder.addKpi("birko", 1234)
+builder.addKpi("cirko", 1234)
+builder.addKpi("sirko", 1234)
 
-pipeline = builder.createPipeline();
-pipeline.setKey("testpipeline");
-pipeline.setDate();
-pipeline.addSlice("China", 123);
-pipeline.addSlice("EU", 3321);
-pipeline.addSlice("USA", 9876);
-builder.addWidget(pipeline);
+//Add a pipeline
+pipeline = builder.createPipeline()
+pipeline.setKey("testpipeline")
+pipeline.setDate(); //Mandatory for now
+pipeline.addSlice("China", 123)
+pipeline.addSlice("EU", 3321)
+pipeline.addSlice("USA", 9876)
+builder.addWidget(pipeline)
+
+//Add a funnel
+funnel = builder.createFunnel()
+funnel.setKey("testfunnel")
+funnel.setDate(); //Mandatory for now
+funnel.addSlice("Pizza", 123)
+funnel.addSlice("Kebab", 3321)
+funnel.addSlice("Burger", 9876)
+builder.addWidget(funnel)
+
+//Add "messages" widget
+messages = builder.createMessages()
+messages.setKey("testmessages")
+messages.setDate(); //Mandatory for now
+messages.addMessage('Woohoo!', 'USD')
+messages.addMessage('Boom!', 'Number')
+messages.addMessage('Yaay!', 'Ticket')
+builder.addWidget(messages)
+
+//Add a pie
+pie = builder.createPie()
+pie.setKey("testpie")
+pie.setDate(); //Mandatory for now
+pie.addSlice("Meat", 123)
+pie.addSlice("Veggies", 3321)
+pie.addSlice("Cheese", 9876)
+builder.addWidget(pie)
+
+//Add a progress indicator
+progress = builder.createProgress()
+progress.setKey("testpie")
+progress.setDate() //Mandatory for now
+progress.setData("Data downloaded", 20, 100)
+builder.addWidget(progress)
+
+//Add a table
+table = builder.createTable()
+table.setKey("testpie")
+table.setDate() //Mandatory for now
+table.addColumn("Username")
+table.addColumn("Downloads")
+table.addRow([{"Jacob", 20}, {"Marco", 40}, {"Jonny", 70}], [{"", 12}, {"", 24}, {"", 36}])
+builder.addWidget(table)
 
 client.pushData(builder);
 
 client.getLog();
+
+//Returned on succesful push
+client.on('dtbxPushSuccess', function(data) {
+	console.log(data)
+})
+
+//Returned when getLog() returns data
+client.on('dtbxLogSuccess', function(data) {
+	console.log(data)
+})
+
+//Returned when an unknown error occurs
+client.on('dtbxUnknownError', function(data) {
+	console.log(data)
+})
+
+//Returned when you make too many requests
+client.on('dtbxTooManyRequests', function(data) {
+	console.log(data)
+	setTimeout(function() {
+		console.log("Try again now!")
+	}, 10000)
+})
